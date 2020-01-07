d82d171f0aff2d410518ac1ffe6ff2743b736058
alonlahav123 <alonlahav123@gmail.com>
2020-01-07 10:34:51 +0200

    updated organization

--- hasalon-app/src/App.js
+++ hasalon-app/src/App.js
@@ -1,17 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DB from "./firestore";
 
 export default function App() {
-  let [counter, setCounter] = useState(0);
-
-  DB.collection('simpleData').doc('persons').onSnapshot((doc) => {
-    console.log(doc.data().amountOfPeople);
-    setCounter( doc.data().amountOfPeople );
-  });
 
   return (
-    <div>
-      {counter}
-    </div>
+    <div></div>
   );
 }
