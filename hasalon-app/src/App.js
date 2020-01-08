c182558e26bbe1dafefcec3f37340eb06cffa8cc
joshua183 <57347220+joshua183@users.noreply.github.com>
2020-01-08 08:09:53 -0600

    yay

--- hasalon-app/src/App.js
+++ hasalon-app/src/App.js
@@ -6,43 +6,32 @@ import DB from "./firestore.js";
 import CapacityCounter from "./components/capacityCounter";
 import CapacityButton from './components/capacityButton';
 import ChairOccupancy from './components/chairCapacityBox';
+import chairs from './components/chairCapacityBox'
 
 export default function App() {
-
-
-//   let Chairs= [
-//     {chair:"chair0", occupiedSeat: occupiedSeat},
-//     {chair:"chair1", occupiedSeat: occupiedSeat},
-//     {chair:"chair2", occupiedSeat: occupiedSeat},
-//     {chair:"chair3", occupiedSeat: occupiedSeat},
-//     {chair:"chair4", occupiedSeat: occupiedSeat},
-//     {chair:"chair5", occupiedSeat: occupiedSeat},
-//     {chair:"chair6", occupiedSeat: occupiedSeat},
-//     {chair:"chair7", occupiedSeat: occupiedSeat},
-//     {chair:"chair8", occupiedSeat: occupiedSeat},
-//     {chair:"chair9", occupiedSeat: occupiedSeat},
-//     {chair:"chair10", occupiedSeat: occupiedSeat},
-//     {chair:"chair11", occupiedSeat: occupiedSeat},
-//     {chair:"chair12", occupiedSeat: occupiedSeat},
-//     {chair:"chair13", occupiedSeat: occupiedSeat},
-//     {chair:"chair14", occupiedSeat: occupiedSeat},
-//     {chair:"chair15", occupiedSeat: occupiedSeat},
-//     {chair:"chair16", occupiedSeat: occupiedSeat},
-//     {chair:"chair17", occupiedSeat: occupiedSeat},
-//     {chair:"chair18", occupiedSeat: occupiedSeat},
-//     {chair:"chair19", occupiedSeat: occupiedSeat},
-//     {chair:"chair20", occupiedSeat: occupiedSeat},
-//     {chair:"chair21", occupiedSeat: occupiedSeat},
-//     {chair:"chair22", occupiedSeat: occupiedSeat}
-// ];
-
+  // const [seatingchart,setSeatingChart]= useState(chairs)
+  
   return (
     <div>
       <CapacityButton multiplier={-1} sign={'-'}/>
       <CapacityButton multiplier={1} sign={'+'}/>
       <CapacityCounter />
-      <ChairOccupancy />
-      <div>
+      <ChairOccupancy 
+      // startingOccupancy=
+        // {
+          // Promise.resolve().
+          // DB.collection('simpleData').doc('persons').get().then((doc)=>{
+            // return doc.data().amountOfPeople 
+          // })
+        // }
+      />
+      
+      <div className='seatingChart'>
+        {/* {
+           seatingchart.map((chair, taken,index)=>{
+            return(<ChairOccupancy key={index} occupied={taken} chair={chair} /> )
+          })    
+        } */}
       </div>
     </div>
   );
--- hasalon-app/src/components/chairCapacityBox.js
+++ hasalon-app/src/components/chairCapacityBox.js
@@ -1,62 +1,162 @@
-import React, { useState } from 'react';
-import './capacityCounter.css';
-import 'firebase/firestore';
+import React, { useState } from "react";
+import "./capacityCounter.css";
+import "firebase/firestore";
 import DB from "../firestore";
 
-function SelectRandomSeat(props) {
-    let chairX;
-    chairX=Math.floor(Math.random()*(21-0)+0);
-    return(chairX)
+function SelectRandomSeat() {
+  let chairX;
+  chairX = Math.floor(Math.random() * (21 - 0) + 0);
+  return chairX;
 }
 
 function ChairOccupancy(props) {
-    const [peopleInThePlaceNow, setPeopleInThePlaceNow] = useState(0);
-    const [populationChange, setPopulationChange]= useState(false);
-    const [occupiedSeat, setOccupiedSeat]= useState(false);
-    let Chairs= [
-        {chair:"chair0", occupiedSeat: occupiedSeat},
-        {chair:"chair1", occupiedSeat: occupiedSeat},
-        {chair:"chair2", occupiedSeat: occupiedSeat},
-        {chair:"chair3", occupiedSeat: occupiedSeat},
-        {chair:"chair4", occupiedSeat: occupiedSeat},
-        {chair:"chair5", occupiedSeat: occupiedSeat},
-        {chair:"chair6", occupiedSeat: occupiedSeat},
-        {chair:"chair7", occupiedSeat: occupiedSeat},
-        {chair:"chair8", occupiedSeat: occupiedSeat},
-        {chair:"chair9", occupiedSeat: occupiedSeat},
-        {chair:"chair10", occupiedSeat: occupiedSeat},
-        {chair:"chair11", occupiedSeat: occupiedSeat},
-        {chair:"chair12", occupiedSeat: occupiedSeat},
-        {chair:"chair13", occupiedSeat: occupiedSeat},
-        {chair:"chair14", occupiedSeat: occupiedSeat},
-        {chair:"chair15", occupiedSeat: occupiedSeat},
-        {chair:"chair16", occupiedSeat: occupiedSeat},
-        {chair:"chair17", occupiedSeat: occupiedSeat},
-        {chair:"chair18", occupiedSeat: occupiedSeat},
-        {chair:"chair19", occupiedSeat: occupiedSeat},
-        {chair:"chair20", occupiedSeat: occupiedSeat},
-        {chair:"chair21", occupiedSeat: occupiedSeat},
-        {chair:"chair22", occupiedSeat: occupiedSeat}
-    ];
-    DB.collection('simpleData').doc('persons').onSnapshot((doc) => {
-        setPopulationChange(true);
-        setPeopleInThePlaceNow(doc.data().amountOfPeople);
-        console.log(peopleInThePlaceNow);
-        for (let index = 0; index < peopleInThePlaceNow; index++) {
-            console.log(Chairs[SelectRandomSeat()].occupiedSeat);
-            Chairs[SelectRandomSeat()].occupiedSeat? (index--):(setOccupiedSeat(true));
-        }
-        console.log(Chairs);
+  const [peopleInThePlaceNow, setPeopleInThePlaceNow] = useState(0);
+// let stateSetter=0;
+    // console.log(startingOccupancy);
+    // console.log('s')
+//   const [peopleInThePlaceNow, setPeopleInThePlaceNow] = useState(startingOccupancy);
+
+//   Idan's until next comment 
+  let initialChairs = [
+    { chair: "chair0", taken: false },
+    { chair: "chair1", taken: false },
+    { chair: "chair2", taken: false },
+    { chair: "chair3", taken: false },
+    { chair: "chair4", taken: false },
+    { chair: "chair5", taken: false },
+    { chair: "chair6", taken: false },
+    { chair: "chair7", taken: false },
+    { chair: "chair8", taken: false },
+    { chair: "chair9", taken: false },
+    { chair: "chair10", taken: false },
+    { chair: "chair11", taken: false },
+    { chair: "chair12", taken: false },
+    { chair: "chair13", taken: false },
+    { chair: "chair14", taken: false },
+    { chair: "chair15", taken: false },
+    { chair: "chair16", taken: false },
+    { chair: "chair17", taken: false },
+    { chair: "chair18", taken: false },
+    { chair: "chair19", taken: false },
+    { chair: "chair20", taken: false },
+    { chair: "chair21", taken: false }
+  ];
+  const [chairs, setChairs] = useState(initialChairs);
+  const setOccupiedSeat = (seatNumber) => {
+      const newChairs = [...chairs]
+      newChairs[seatNumber] = {...newChairs[seatNumber], taken: true}
+      setChairs(newChairs)
+  }
+  
+
+//   everytime setOccupiedSeat is called the page will function will be called again with the new state
+    // so all occupiedSeats will be turned to true b/c they all reference the same reloader changed variable
+    // const [occupiedSeat, setOccupiedSeat]= useState(false);
+    // let Chairs=[
+    //     { chair: "chair0", taken: occupiedSeat },
+    //     { chair: "chair1", taken: occupiedSeat },
+    //     { chair: "chair2", taken: occupiedSeat },
+    //     { chair: "chair3", taken: occupiedSeat },
+    //     { chair: "chair4", taken: occupiedSeat },
+    //     { chair: "chair5", taken: occupiedSeat },
+    //     { chair: "chair6", taken: occupiedSeat },
+    //     { chair: "chair7", taken: occupiedSeat },
+    //     { chair: "chair8", taken: occupiedSeat },
+    //     { chair: "chair9", taken: occupiedSeat },
+    //     { chair: "chair10", taken: occupiedSeat },
+    //     { chair: "chair11", taken: occupiedSeat },
+    //     { chair: "chair12", taken: occupiedSeat },
+    //     { chair: "chair13", taken: occupiedSeat },
+    //     { chair: "chair14", taken: occupiedSeat },
+    //     { chair: "chair15", taken: occupiedSeat },
+    //     { chair: "chair16", taken: occupiedSeat },
+    //     { chair: "chair17", taken: occupiedSeat },
+    //     { chair: "chair18", taken: occupiedSeat },
+    //     { chair: "chair19", taken: occupiedSeat },
+    //     { chair: "chair20", taken: occupiedSeat },
+    //     { chair: "chair21", taken: occupiedSeat }
+    // ]
+//     let Chairs=[
+//     { chair: "chair0", taken: false },
+//     { chair: "chair1", taken: false },
+//     { chair: "chair2", taken: false },
+//     { chair: "chair3", taken: false },
+//     { chair: "chair4", taken: false },
+//     { chair: "chair5", taken: false },
+//     { chair: "chair6", taken: false },
+//     { chair: "chair7", taken: false },
+//     { chair: "chair8", taken: false },
+//     { chair: "chair9", taken: false },
+//     { chair: "chair10", taken: false },
+//     { chair: "chair11", taken: false },
+//     { chair: "chair12", taken: false },
+//     { chair: "chair13", taken: false },
+//     { chair: "chair14", taken: false },
+//     { chair: "chair15", taken: false },
+//     { chair: "chair16", taken: false },
+//     { chair: "chair17", taken: false },
+//     { chair: "chair18", taken: false },
+//     { chair: "chair19", taken: false },
+//     { chair: "chair20", taken: false },
+//     { chair: "chair21", taken: false }
+//   ];
+    // console.log(chairs);
+//   console.log(Chairs);
+  DB.collection("simpleData")
+    .doc("persons")
+    .onSnapshot(doc => {
+      setPeopleInThePlaceNow(doc.data().amountOfPeople);
+    //   console.log(peopleInThePlaceNow);
+    //   OccupiedChairs.length=peopleInThePlaceNow;
+      for (let index = 0; index < peopleInThePlaceNow; index++) {
+            const randomSeat = SelectRandomSeat()
+            // console.log(chairs[randomSeat].occupiedSeat);
+        // let occupiedChairId=SelectRandomSeat();
+            chairs[randomSeat].occupiedSeat
+        // Chairs[occupiedChairId].taken
+        ? index--
+        // : Chairs[occupiedChairId].taken=true
+            : setOccupiedSeat(randomSeat);
+        // : setOccupiedSeat(true);
+      }
+    //   console.log(Chairs);
+        // console.log(chairs);
     });
 
-    return(
-        <div className='container'>
-            Chairs.forEach(chair => {
-                occupiedSeat? (<div className='occupied seat'></div>):(<div className='open seat'></div>)
-            });
-            
-        </div> 
-    )    
+  return (
+      
+    <div className="container" onclick=
+      {
+          chairs.forEach(chair => 
+        chair.occupiedSeat ? (
+            <div className="occupied seat"></div>
+          ) : (
+            <div className="open seat"></div>
+          )
+        
+        // Chairs.forEach(chair=>chair.occupiedSeat ? (
+        //     <div className="occupied seat">hb</div>
+        //   ) : (
+        //     <div className="open seat">bj</div>
+        //   )
+        // )
+        )
+      }>fds</div>
+
+
+        // // If I want to have a return:
+        // Chairs.forEach(chair =>{
+        //  return   chair.occupiedSeat ? (<div className="occupied seat"></div>) : (<div className="open seat"></div>)
+        // })
+        // If I want to have a return:
+        // Chairs.forEach(chair =>{
+        //     if (chair.occupiedSeat) {
+        //        return (<div className="occupied seat"></div>)
+        //     } else {
+        //         return (<div className="open seat"></div>)
+        //     }
+        // })
+  );
 }
 
 export default ChairOccupancy;
