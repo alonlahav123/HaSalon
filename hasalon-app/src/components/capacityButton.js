b43b09d0271c8140ae31431298e54c119a713369
alonlahav123 <alonlahav123@gmail.com>
2020-01-09 11:45:06 +0200

    updated the rows/cols of front desk page

--- hasalon-app/src/components/capacityButton.js
+++ hasalon-app/src/components/capacityButton.js
@@ -6,7 +6,7 @@ import firebase from 'firebase/app';
 
 function capacityButton(props) {
     return (
-        <Button className='btn' onClick={() => {updateCapacity(props.multiplier)}} variant='primary' active>{props.sign}</Button>
+        <Button className='btn' onClick={() => {updateCapacity(props.multiplier)}} variant='primary' size='lg' block>{props.sign}</Button>
     )
 
 }
@@ -15,12 +15,13 @@ function updateCapacity(multiplier) {
 
     DB.collection('simpleData').doc('persons').get().then( function(doc) {
         let amntPeople = doc.data().amountOfPeople;
-        
+
         if(amntPeople > 0 || multiplier >= 0) {
             DB.collection('simpleData').doc('persons').update({
                 amountOfPeople: firebase.firestore.FieldValue.increment(multiplier)
             });
         } 
+        
     })
 }
 
--- hasalon-app/src/pages/frontDesk.js
+++ hasalon-app/src/pages/frontDesk.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './frontDesk.css'
+import {Container, Col, Row} from 'react-bootstrap';
+
 
 import CapacityCounter from "../components/capacityCounter";
 import CapacityButton from '../components/capacityButton';
@@ -7,17 +9,27 @@ import FrontDeskCalendar from '../components/frontDeskCalendar'
 
 function frontDesk() {
     return (
-        <div>
-            <h2>Front Desk</h2>
-            <div className='container'>
-                <div className='capacityContainer'>
+        <Container>
+            <Row>
+                <Col>
+                    <h2>Front Desk</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={2}>
                     <CapacityButton multiplier={-1} sign={'-'} />
+                </Col>
+                <Col xs={2}>
                     <CapacityCounter />
+                </Col>
+                <Col xs={2}>
                     <CapacityButton multiplier={1} sign={'+'} />
-                </div>
-                <FrontDeskCalendar className='calendar' />
-            </div>
-        </div>
+                </Col>
+                <Col xs={6}>
+                    <FrontDeskCalendar className='calendar' />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
