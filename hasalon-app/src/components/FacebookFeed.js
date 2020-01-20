1694cacc81b45ac4cffd738727aef4944e5facdc
Alon Lahav <33399838+alonlahav123@users.noreply.github.com>
2020-01-20 11:54:49 +0200

    Update FacebookFeed.js

--- hasalon-app/src/components/FacebookFeed.js
+++ hasalon-app/src/components/FacebookFeed.js
@@ -6,7 +6,6 @@ export  class FacebookFeed extends Component {
     return (
       <FacebookProvider appId="2184589704978013">
         <Page href="https://www.facebook.com/hasaloncop/?eid=ARCBchcJq9DokkpN3bobIhSuw7gRxkvi3FwWqcQ7HbBjAqOzdQF_owRDWa-UDIm_HdfQW8vdCTRfJV2k" tabs="timeline" />
-      {/* https://www.facebook.com/pg/hasaloncop/events/?ref=page_internal */}
       </FacebookProvider>    
     );
   }
