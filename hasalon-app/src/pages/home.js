5c989eec6915566e87c4597963d10aa8c5827e79
joshua183 <57347220+joshua183@users.noreply.github.com>
2020-01-22 05:13:30 -0600

    homePage updates

--- hasalon-app/src/pages/home.js
+++ hasalon-app/src/pages/home.js
@@ -3,31 +3,48 @@ import FacebookFeed from '../components/FacebookFeed';
 import FacebookEvent from '../components/facebookEvents';
 import { Container, Col, Row } from "react-bootstrap";
 import CapacityBar from '../components/capacityBar';
+import './home.css';
 
 
 function home() {
     return (
         <div>
             <Container fluid='true'>
-                <Row>
-                    <Col></Col>
-                    <Col>
+                <Row 
+                className=' '
+                >
+                    <Col xs={1} lg={2}></Col>
+                    <Col xs={9} lg={8}
+                    className='colContainer align-items-center'
+                    >
                         <h2>Project: ReSalon</h2>
                     </Col>
-                    <Col></Col>
+                    <Col xs={1} lg={2}></Col>
                 </Row>
-                <Row className="rowContainer align-items-center">
+                <Row className="rowHeight align-items-center">
                     <Col>
                         <CapacityBar/>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <FacebookFeed/>
-                    </Col>
-                    <Col>
-                        <FacebookEvent/>
-                    </Col>
+                    <Container>
+                        <Row className='FBstacker'>
+                            {/* <Col 
+                            xs={6}
+                            >
+                                 <Container fluid='true'>
+                                 <FacebookFeed/>
+                                 </Container> 
+                            </Col> */}
+                            <Col className='center'
+                            xs={6}
+                            >
+                                {  /* <Container fluid='true'> */}
+                                    <FacebookEvent/>
+                                {/* </Container> */}
+                            </Col>
+                        </Row>
+                    </Container>
                 </Row>
             </Container>
         </div>
