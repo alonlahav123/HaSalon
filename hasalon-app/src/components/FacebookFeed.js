a373aee28ad600afb1c3892c5c69c5b6be45e3ac
alonlahav123 <alonlahav123@gmail.com>
2020-01-26 09:50:36 +0200

    deleted code that was not merged correctly

--- hasalon-app/src/components/FacebookFeed.js
+++ hasalon-app/src/components/FacebookFeed.js
@@ -7,7 +7,6 @@ export  class FacebookFeed extends Component {
   render(WidthFinder) {
     console.log(WidthFinder)
     return (
-<<<<<<< HEAD
       <div className='faceBook'>
         <FacebookProvider appId="2184589704978013">
           <Page 
@@ -18,14 +17,8 @@ export  class FacebookFeed extends Component {
       {/* https://www.facebook.com/pg/hasaloncop/events/?ref=page_internal */}
         </FacebookProvider>    
       </div>
-=======
-      <FacebookProvider appId="2184589704978013">
-        <Page href="https://www.facebook.com/hasaloncop/?eid=ARCBchcJq9DokkpN3bobIhSuw7gRxkvi3FwWqcQ7HbBjAqOzdQF_owRDWa-UDIm_HdfQW8vdCTRfJV2k" tabs="timeline" />
-      </FacebookProvider>    
->>>>>>> 7a8e9ceff77690b4d93afce4828d55f0b56a7129
     );
   }
 }
-
 
 export default FacebookFeed;
