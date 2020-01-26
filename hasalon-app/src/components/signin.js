f420383f4efb22746d0c00b84db3a3727f03b636
alonlahav123 <alonlahav123@gmail.com>
2020-01-26 11:47:38 +0200

    css for sign in and registration

--- hasalon-app/src/components/signin.js
+++ hasalon-app/src/components/signin.js
@@ -1,71 +1,68 @@
 import React, { useState } from "react";
 import "./capacityCounter.css";
 import "firebase/firestore";
-import {
-  Form,
-  Button,
-  Card,
-} from "react-bootstrap";
+import { Form, Button, Card} from "react-bootstrap";
 import * as firebase from "firebase";
 
 function SignInForm(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-          <Form onSubmit={onSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                value={email}
-                onChange={event => setEmail(event.target.value)}
-                type="email"
-                placeholder="Enter email"
-              />
-            </Form.Group>
+    <Form onSubmit={onSubmit}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          type="email"
+          placeholder="Enter email"
+        />
+      </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-                type="password"
-                placeholder="Password"
-                minLength="6"
-              /> 
-            </Form.Group>
-            
-              <a href="/forgot">
-              <Form.Text>
-                Don't remember your password? Click here to reset.
-              </Form.Text>
-              </a>
-              
-              <br />
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+          type="password"
+          placeholder="Password"
+          minLength="6"
+        />
+      </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+      <a href="/forgot">
+        <Form.Text>
+          Don't remember your password? Click here to reset.
+        </Form.Text>
+      </a>
+
+      <br />
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
   function onSubmit(event) {
     event.preventDefault();
     console.log({ email }, { password });
-  
+
     logInWithEmailAndPassword(email, password);
-}
+  }
 }
 
 function logInWithEmailAndPassword(email, password) {
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert(errorMessage)
-    // ...
-  });
-  alert("Sucess")
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+      // ...
+    });
+  alert("Sucess");
 }
-
 
 export default SignInForm;
